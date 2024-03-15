ae08800863e1eb140b813e10049cb7c09e67a18a
mishxnz <149193122+mishxnz@users.noreply.github.com>
2024-03-15 16:57:04 +0000

    first code

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(void) {
-  printf("Hello World\n");
+using namespace std;
+
+int main() {
+     const int size = 5;
+
+    int array(size) = {1, 2, 3, 4, 5};
+
+    cout << "Елементи масиву:" <<endl;
+    for(int i = 0; i < size; ++i) {
+        cout << array[i] << " ";
+  }
+  cout << endl;
+
   return 0;
 }
+// Misha Simbirtsev SKIBIDI
